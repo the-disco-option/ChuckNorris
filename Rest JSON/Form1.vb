a144929fa40fd302f589e54e6e612094dcea3fe7
gamingexpx12 <gamingexpx12@gmail.com>
2015-12-14 14:50:20 +0100

    Implemented Z_JSON

--- Rest JSON/Form1.vb
+++ Rest JSON/Form1.vb
@@ -4,6 +4,7 @@ Imports System.Net
 Imports System.Text
 Imports Newtonsoft.Json
 Imports Newtonsoft.Json.Linq
+Imports ZEKE_JSON
 
 Public Class Form1
 
@@ -21,33 +22,13 @@ Public Class Form1
     End Class
 
     Private Sub JSONWEBTEST()
+        Dim URL As String = "http://api.icndb.com/jokes/random"
         Dim Chuck As New Joke
-        ' Create a request for the URL. 
-        Dim request As WebRequest = WebRequest.Create("http://api.icndb.com/jokes/random")
 
-        ' If required by the server, set the credentials.
-        request.Credentials = CredentialCache.DefaultCredentials
-        ' Get the response.
-        Dim response As WebResponse = request.GetResponse()
-        ' Display the status.
-        Console.WriteLine(CType(response, HttpWebResponse).StatusDescription)
-        ' Get the stream containing content returned by the server.
-        Dim dataStream As Stream = response.GetResponseStream()
-        ' Open the stream using a StreamReader for easy access.
-        Dim reader As New StreamReader(dataStream)
-        ' Read the content.
-        Dim responseFromServer As String = reader.ReadToEnd()
-        'Dim responseFromServer As String = reader.ReadToEnd()
-        Console.WriteLine(responseFromServer)
-        'HACK We don't need a list or an array
-        Chuck = JsonConvert.DeserializeObject(Of Joke)(responseFromServer)
-        'Dim result = serial.Deserialize(responseFromServer)
+        Chuck = Z_JSON.WebToObject(Of Joke)(URL)
         ContentBox.Text = Chuck.value.joke
         ContentBox.Text = ContentBox.Text.Replace("&quot;", "'")
         JokeID.Text = "Joke: " & Chuck.value.id
-        ' Clean up the streams and the response.
-        reader.Close()
-        response.Close()
     End Sub
 
 
---
+++ ZEKE_JSON/Z_JSON.vb
@@ -0,0 +1,56 @@
+﻿Imports System
+Imports System.IO
+Imports System.Net
+'Imports System.Text
+Imports Newtonsoft.Json
+'Imports Newtonsoft.Json.Linq
+''' <summary>
+''' Object for easy handling of JSON
+''' </summary>
+''' <remarks></remarks>
+Public Module Z_JSON
+
+    Public Function WebToObject(Of t)(URL As String)
+        ' Create a request for the URL. 
+        Dim request As WebRequest = WebRequest.Create(URL)
+        ' If required by the server, set the credentials.
+        request.Credentials = CredentialCache.DefaultCredentials
+        ' Get the response.
+        Dim response As WebResponse = request.GetResponse()
+        ' Display the status.
+        Console.WriteLine(CType(response, HttpWebResponse).StatusDescription)
+        ' Get the stream containing content returned by the server.
+        Dim dataStream As Stream = response.GetResponseStream()
+        ' Open the stream using a StreamReader for easy access.
+        Dim reader As New StreamReader(dataStream)
+        ' Read the content.
+        Dim responseFromServer As String = reader.ReadToEnd()
+        'Dim responseFromServer As String = reader.ReadToEnd()
+        Console.WriteLine("WebResponse: " & responseFromServer)
+        ' Clean up the streams and the response.
+        reader.Close()
+        response.Close()
+
+        ' Return the object.
+        Return JsonConvert.DeserializeObject(Of t)(responseFromServer)
+
+    End Function
+    ''' <summary>
+    ''' Assembles argumnets belonging to a URL
+    ''' </summary>
+    ''' <param name="argDictionary"></param>
+    ''' <returns>?arg1=1;arg2</returns>
+    ''' <remarks>Not Implemented Yet</remarks>
+    Private Function ArgAssebly(ByVal argDictionary As Dictionary(Of String, String)) As String
+        ' Make Dictionary into a list
+        Dim argList As List(Of KeyValuePair(Of String, String)) = argDictionary.ToList()
+        'Dim pairList As List(Of String)
+        For Each pair As KeyValuePair(Of String, String) In argList
+            Dim content As String = pair.Key & "="
+            'pairList.Add(content)
+        Next
+
+        Throw New NotImplementedException
+        Return Nothing
+    End Function
+End Module
